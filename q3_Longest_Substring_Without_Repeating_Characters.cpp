543744664009248a2a5e0270e261022f793972fd
sentry5588 <hjlgnx16@gmail.com>
2021-03-10 22:38:22 -0500

    Update q3_Longest_Substring_Without_Repeating_Characters.cpp
    Runtime: 16 ms, faster than 70.58% of C++ online submissions for Longest Substring Without Repeating Characters.
    Memory Usage: 7.8 MB, less than 79.34% of C++ online submissions for Longest Substring Without Repeating Characters.

--- q3_Longest_Substring_Without_Repeating_Characters.cpp
+++ q3_Longest_Substring_Without_Repeating_Characters.cpp
@@ -1,19 +1,27 @@
 https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/
 
+/**
+ * Better, still need to improve
+ * 
+ * Runtime: 16 ms, faster than 70.58% of C++ online submissions for Longest Substring Without Repeating Characters.
+ * Memory Usage: 7.8 MB, less than 79.34% of C++ online submissions for Longest Substring Without Repeating Characters. 
+ */
+ 
 #include <algorithm>
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         size_t l = 0;
+        deque<char> q;
         for (auto i = s.begin(); i != s.end(); ++i ) {
-            unordered_set<char> string_set = {*i};
-            auto j = i+1;
-            while ((string_set.find(*j) == string_set.end()) && (j != s.end()) ) {
-                string_set.insert(*j);
-                ++j;
+            auto r = std::find(q.begin(), q.end(), *i);
+            if (r != q.end()) {
+                q.erase(q.begin(), r);
+                q.erase(r);
             }
-            l = std::max(l, string_set.size());
+            q.push_back(*i);
+            l = std::max(l, q.size());
         }
         return l;
     }

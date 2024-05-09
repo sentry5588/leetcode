ee6d52a0945c1249bbf06090cfc50dbcdd4ca989
sentry5588 <49176772+sentry5588@users.noreply.github.com>
2024-05-09 17:48:24 -0400

    Q 88, add two more solutions

--- q088_merge_sorted_array.cpp
+++ q088_merge_sorted_array.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        /* ================================================================
+         * 3rd solution */
+        int i = m + n - 1;
+        int i1 = m - 1;
+        int i2 = n - 1;
+        while (i1 >= 0 && i2 >= 0) { // both list non empty
+            if (nums1[i1] >= nums2[i2]) {
+                nums1[i] = nums1[i1];
+                i1--;
+            } else {
+                nums1[i] = nums2[i2];
+                i2--;
+            }
+            i--;
+        }
+
+        while (i2 >= 0) {
+            nums1[i] = nums2[i2];
+            i2--;
+            i--;
+        }
+
+        /* ================================================================
+         * 2nd solution
+        for (int i = 0; i < n; ++i) {
+            nums1[m + i] = nums2[i]; // Append nums2 to nums1
+        }
+        sort(nums1.begin(), nums1.end()); */
+        
+        /* ================================================================
+         * First solution 
         int p1 = 0, p2 = 0; // Pointer to the current elements
         vector<int> nums; // TODO: how to avoid creating a new array
 
@@ -28,6 +59,6 @@ public:
 
         for (int i = 0; i < m + n; ++i) {
             nums1[i] = nums[i];
-        }
+        } */
     }
 };

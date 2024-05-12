0fea4fe65f29d59bf20021f044586e7211b79893
sentry5588 <49176772+sentry5588@users.noreply.github.com>
2024-05-12 10:23:33 -0400

    ### Learn: Moore voting algo

--- q_169_majority_element.py
+++ q_169_majority_element.py
@@ -4,10 +4,29 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+
+        ##################################################
+        # Solution 3, moore voting algo
+        candidate = nums[0]
+        vote = 1
+        for n in nums:
+            if n == candidate:
+                vote += 1
+            else:
+                vote -= 1
+                if vote == 0:
+                    candidate = n
+                    vote = 1
+
+        return candidate
+
+        ##################################################
+        
         ##################################################
         # Solution 2, use sort first, time O(n log n), space O(1)
-        nums.sort()
-        n_maj = nums[len(nums)//2]
+        # nums.sort()
+        # n_maj = nums[len(nums)//2]
+        # return n_maj
         ##################################################
 
         ##################################################
@@ -21,6 +40,7 @@ class Solution(object):
         #             n_maj = n
         #     else:
         #         count[n] = 1
+        # return n_maj
         ##################################################
 
-        return n_maj
+        

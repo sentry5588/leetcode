296d32c5c4f04c9894771b8aa3458b18cdc44440
sentry5588 <49176772+sentry5588@users.noreply.github.com>
2024-07-05 00:22:33 -0400

    Q 100, improve the recursive solution

--- q_100_same_tree_recursive.cpp
+++ q_100_same_tree_recursive.cpp
@@ -12,20 +12,17 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) {
+            return true;
+        }
         if ( (p == nullptr && q != nullptr) || (p != nullptr && q == nullptr) ) {
             return false;
-        } else if (p != nullptr && q != nullptr) {
-            if (p->val == q->val) {
-                if (isSameTree(p->left, q->left) == false) {
-                    return false;
-                }
-                if (isSameTree(p->right, q->right) == false) {
-                    return false;
-                }
-            } else {
+        } 
+        if (p != nullptr && q != nullptr) {
+            if (p->val != q->val) {
                 return false;
             }
         }
-        return true;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
